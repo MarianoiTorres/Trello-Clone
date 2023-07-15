b85f1eab8fdac6a3570a2188f9a94a45c4ef0221
Mariano Torres <marianxtorres@gmail.com>
2023-07-15 01:53:58 -0300

    angular

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    TasksComponent,
+    HomePageComponent,
+    ProjectPageComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
